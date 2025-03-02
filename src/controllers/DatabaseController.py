a986f278568ecb2530febe7e24ea968142b0a379
Matheus Henrique <matheus1604.mh@gmail.com>
2025-03-01 22:17:57 -0300

    🔨 refactor(controllers): rename DatabaseController to DatabaseCrud and add select method

--- src/controllers/DatabaseController.py
+++ src/controllers/DatabaseController.py
@@ -1,12 +1,13 @@
+from typing import Any, Dict, List
+
 import streamlit as st
 from supabase import create_client
-from typing_extensions import List
 
-from src.models.schemas import Business, Expense, PurchasedItem
+from src.models.finance_data_models import Business, Expense, PurchasedItem
 
 
 @st.cache_resource
-class DatabaseController:
+class DatabaseCrud:
     def __init__(self):
         self.client = create_client(
             supabase_url=st.secrets["SUPABASE_URL"],
@@ -54,5 +55,19 @@ class DatabaseController:
             print("Generated SQL:", query.explain())
         response = query.execute()
         business_id = response.data[0]["id"]
-
         return business_id
+
+    def select(
+        self,
+        table_name: str,
+        columns: List[str] = ["*"],
+        condition: Dict[str, Any] = None,
+        show_sql=False,
+    ):
+        query = self.client.table(table_name).select(",".join(columns))
+        if condition:
+            for key, value in condition.items():
+                query = query.eq(key, value)
+        if show_sql:
+            print("Generated SQL:", query.explain())
+        return query.execute()
